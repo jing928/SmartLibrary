3571c2e920287c46b2ab461a4260f55383c8c545
Jing Li <lyovse@gmail.com>
2019-05-03 17:35:42 +1000

    Add initial user registration class

--- rp_app/user_registration.py
+++ rp_app/user_registration.py
@@ -1,7 +1,49 @@
+from rp_app.data_access_local import DataAccess
+from rp_app.validator import Validator
+
+
 class UserRegistration:
 
     def __init__(self):
-        pass
+        self.__dao = DataAccess()
+        self.__new_username = None
+        self.__new_fullname = None
+        self.__new_password = None
 
     def start(self):
-        pass
+        self.ask_for_username()
+        self.ask_for_fullname()
+        self.ask_for_password()
+        self.register_user()
+
+    def ask_for_username(self):
+        is_valid = False
+        while not is_valid:
+            username = input('--> Enter the username here:\n'
+                             '(Only letters and numbers are allowed, minimum 4 characters)')
+            username = username.strip()
+            is_valid = Validator.validate_username(username)
+            if not is_valid:
+                print('Username entered does not meet the requirements...\n')
+        self.__new_username = username
+
+    def ask_for_fullname(self):
+        is_valid = False
+
+    def ask_for_password(self):
+        is_valid = False
+
+    def register_user(self):
+        if self.__new_username is None:
+            print("Error: username wasn't saved correctly...")
+            return
+        if self.__new_fullname is None:
+            print("Error: full name wasn't saved correctly...")
+            return
+        if self.__new_password is None:
+            print("Error: password wasn't saved correctly...")
+            return
+        self.__dao.insert_user(username=self.__new_username,
+                               fullname=self.__new_fullname,
+                               password=self.__new_password)
+        print('Registration successful!')
