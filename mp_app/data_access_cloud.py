3494405a1cc4987b8ab844b6c2cdf59f10c178af
Jing Li <lyovse@gmail.com>
2019-05-08 23:34:36 +1000

    Implement search, borrow, and return data access methods

--- mp_app/data_access_cloud.py
+++ mp_app/data_access_cloud.py
@@ -1,4 +1,7 @@
+from datetime import date
+
 import pymysql as mysql
+
 from utils.file_access import FileAccess
 
 
@@ -28,9 +31,52 @@ class DataAccessCloud():
         self.close()
 
     def search(self, query):
-        search_str = '%' + query + '%'
         cur = self.con.cursor()
+        search_str = '%' + query + '%'
         cur.execute('SELECT * FROM Book '
                     'WHERE ISBN LIKE %s OR Title LIKE %s OR Author LIKE %s',
                     (search_str, search_str, search_str))
         return cur.fetchall()
+
+    def get_user_id(self, username):
+        cur = self.con.cursor()
+        cur.execute('SELECT LmsUserID FROM LmsUser WHERE UserName = %s', username)
+        result = cur.fetchone()
+        if result is None:
+            print("User doesn't exist.")
+            return result
+        return result['LmsUserID']
+
+    def check_availability(self, book_id):
+        cur = self.con.cursor()
+        cur.execute('SELECT Status FROM Book WHERE BookID = %s', book_id)
+        result = cur.fetchone()['Status']
+        if result == 'available':
+            return True
+        if result is None:
+            print("The book doesn't exist.")
+        if result == 'unavailable':
+            print('The book is not available.')
+        return False
+
+    def borrow_book(self, book_id, user_id):
+        cur = self.con.cursor()
+        borrow_date = date.today()
+        cur.execute('INSERT INTO BookBorrowed (LmsUserID, BookID, Status, BorrowedDate)'
+                    'VALUES (%s, %s, %s, %s)', (user_id, book_id, 'borrowed', borrow_date))
+        cur.execute('UPDATE Book SET Status = %s WHERE BookID = %s', ('unavailable', book_id))
+        self.con.commit()
+
+    def return_book(self, book_borrowed_id):
+        cur = self.con.cursor()
+        return_date = date.today()
+        book_id = self.__get_book_id(book_borrowed_id)
+        cur.execute('UPDATE BookBorrowed SET Status = %s, ReturnedDate = %s '
+                    'WHERE BookBorrowedID = %s', ('returned', return_date, book_borrowed_id))
+        cur.execute('UPDATE Book SET Status = %s WHERE BookID = %s', ('available', book_id))
+        self.con.commit()
+
+    def __get_book_id(self, book_borrowed_id):
+        cur = self.con.cursor()
+        cur.execute('SELECT BookID FROM BookBorrowed WHERE BookBorrowedID = %s', book_borrowed_id)
+        return cur.fetchone()['BookID']
