87a15940e052a89b80c9ae349f65c1d82f08d47a
Jing Li <lyovse@gmail.com>
2019-05-10 00:18:39 +1000

    Finish preliminary search and borrow functions

--- mp_app/book_function.py
+++ mp_app/book_function.py
@@ -1,3 +1,4 @@
+from tabulate import tabulate
 from mp_app.data_access_cloud import DataAccessCloud
 
 
@@ -10,3 +11,36 @@ class BookFunction:
         if user_id is None:
             raise Exception('Username does not exist.')
         self.__user_id = user_id
+        self.__search_result = None
+
+    def search(self):
+        query = input('You can search by title, ISBN, or author.\n'
+                      '--> Please enter your search keywords here:')
+        result = self.__dao.search(query)
+        if not result:
+            print('Sorry...there is no book matching the keywords.\n')
+            return
+        self.__search_result = tabulate(result, headers='keys', tablefmt='fancy_grid')
+        print(self.__search_result)
+
+    def borrow(self):
+        if self.__search_result is None:
+            print('Please search for a book first...')
+            return
+        print('** Below is your previous search result **\n')
+        print(self.__search_result)
+        book_id = self.__ask_for_book_id()
+        self.__dao.borrow_book(book_id, self.__user_id)
+        # TODO: add event to Google Calendar
+        print('Book successfully borrowed!')
+
+    def __ask_for_book_id(self):
+        book_found = False
+        while not book_found:
+            input_id = input('--> Please enter the BookID here:')
+            if input_id.isdigit():
+                book_id = int(input_id)
+                book_found = self.__dao.check_availability(book_id)
+            else:
+                print('Error: please only enter a positive integer number.')
+        return book_id
