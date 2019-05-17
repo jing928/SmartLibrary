0a7d3a1bf716e645a2154dc91d9e441e403aa298
DarrenHub <s3641239@student.rmit.edu.au>
2019-05-17 12:36:35 +1000

    Add documentation to rp and mp console

--- mp_app/mp_console.py
+++ mp_app/mp_console.py
@@ -1,12 +1,24 @@
-#!/usr/bin/env python3
-# Reference: https://realpython.com/python-sockets/
-# Documentation: https://docs.python.org/3/library/socket.html
+"""
+This module provides functions to run Master Pi Console Menu.
+"""
+
 import socket
 from mp_app.book_function import BookFunction
 from utils.menu_helper import MenuHelper
 
 
 class MpConsole:
+    """
+    MpConsole class handles the Master Pi Console Menu function.
+
+    Attributes:
+        MSG (str): a default reply message.
+        menu_items ([str]): a list of menu items to print.
+        menu_end_number (int): the choice number for the last selectable item.
+        __username (str, None): the username of the current logged in user.
+        ___book_function (BookFunction, None): the BookFunction object
+    """
+
     MSG = 'logout'
 
     def __init__(self):
@@ -22,7 +34,17 @@ class MpConsole:
         self.__book_function = None
 
     def start(self):
-        """receive username and start Mp program"""
+        """Receives username and starts the Master Pi program
+
+        It will continue listening to port 65000 from all IP addresses and
+        when message received, it will create a new BookFunction instance
+        and run the main menu for the user.
+
+        Once the user logs out, it will go back to listening for new messages.
+
+        Returns: None
+
+        """
         host = ''
         port = 65000
         address = (host, port)
@@ -46,6 +68,14 @@ class MpConsole:
                         self.__book_function = None
 
     def run_menu(self):
+        """Runs the main menu
+
+        It will keep showing the menu and asking for user selection until
+        the user logs out.
+
+        Returns: None
+
+        """
         should_quit = False
         while not should_quit:
             MenuHelper.print_menu(self.menu_items)
@@ -53,6 +83,18 @@ class MpConsole:
             should_quit = self.__handle_choice(choice)
 
     def __handle_choice(self, choice):
+        """Delegates user choice to corresponding class to handle
+
+        Given the user choice, this method will create and call other
+        class methods to handle the function.
+
+        Args:
+            choice: the user's choice of a menu item.
+
+        Returns:
+            bool: True if user chooses to logout, False otherwise.
+
+        """
         if choice == 1:
             self.__book_function.search_for_book()
             return False
