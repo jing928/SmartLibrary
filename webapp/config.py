e3d00c4c1376102e15944fc7b6dec3bd6f717d06
Quan Cheng <s3181520@student.rmit.edu.au>
2019-05-22 13:16:49 +1000

    Add simple graph on analysis page

--- webapp/config.py
+++ webapp/config.py
@@ -22,4 +22,6 @@ class Config(object):
 
     LEND_URL = 'https://datastudio.google.com/embed/reporting/1ni-4ZoFqrTgkxDg1SVKgw7r2gUkvQIgh/page/yYBq'
 
-    RETURN_URL = 'https://datastudio.google.com/embed/reporting/1ni-4ZoFqrTgkxDg1SVKgw7r2gUkvQIgh/page/yYBq'
+    RETURN_URL = 'https://datastudio.google.com/embed/reporting/1xNEctzAzy9-LU1GsN1QKZp7UL6q5Av6c/page/vHPq'
+
+    # RETURN_URL = 'https://datastudio.google.com/open/1xNEctzAzy9-LU1GsN1QKZp7UL6q5Av6c'
