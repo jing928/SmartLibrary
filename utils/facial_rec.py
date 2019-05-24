1a24272091018a66ff03f2aa815ff86419137886
Jing Li <lyovse@gmail.com>
2019-05-24 20:01:16 +1000

    Fix pylint issue

--- utils/facial_rec.py
+++ utils/facial_rec.py
@@ -11,8 +11,8 @@ import pickle
 import time
 import imutils
 from imutils.video import VideoStream
-import face_recognition
 import cv2
+import face_recognition
 
 
 class FacialRecognition:
@@ -23,48 +23,51 @@ class FacialRecognition:
 
     Attributes:
         __username (str, None): username stored in encodings.pickle
-        
     """
+
     def __init__(self):
         self.__username = None
 
     def recognition(self):
-        # construct the argument parser and parse the arguments
-        ap = argparse.ArgumentParser()
-        ap.add_argument("-e", "--encodings", default='encodings.pickle',
-                        help="path to serialized db of facial encodings")
-        ap.add_argument("-r", "--resolution", type=int, default=240,
-                        help="Resolution of the video feed")
-        ap.add_argument("-o", "--output", type=str,
-                        help="path to output video")
-        ap.add_argument("-y", "--display", type=int, default=0,
-                        help="whether or not to display output frame to screen")
-        ap.add_argument("-d", "--detection-method", type=str, default="hog",
-                        help="face detection model to use: either `hog` or `cnn`")
-        args = vars(ap.parse_args())
+        """Recognize the face to find username
 
-        # load the known faces and embeddings
+        Returns:
+            None
+
+        """
+        argument_parser = argparse.ArgumentParser()
+        argument_parser.add_argument("-e", "--encodings", default='encodings.pickle',
+                                     help="path to serialized db of facial encodings")
+        argument_parser.add_argument("-r", "--resolution", type=int, default=240,
+                                     help="Resolution of the video feed")
+        argument_parser.add_argument("-o", "--output", type=str,
+                                     help="path to output video")
+        argument_parser.add_argument("-y", "--display", type=int, default=0,
+                                     help="whether or not to display output frame to screen")
+        argument_parser.add_argument("-d", "--detection-method", type=str, default="hog",
+                                     help="face detection model to use: either `hog` or `cnn`")
+        args = vars(argument_parser.parse_args())
+
+        # Load the known faces and embeddings
         print("[INFO] loading encodings...")
         data = pickle.loads(open(args["encodings"], "rb").read())
 
         # initialize the video stream and pointer to output video file, then
         # allow the camera sensor to warm up
         print("[INFO] starting video stream...")
-        vs = VideoStream(src=0).start()
+        video_stream = VideoStream(src=0).start()
         writer = None
         time.sleep(2.0)
 
         # loop over frames from the video file stream
-        
-        for i in(0,5):
+
+        for _ in (0, 5):
             # grab the frame from the threaded video stream
-            frame = vs.read()
+            frame = video_stream.read()
 
             # convert the input frame from BGR to RGB then resize it to have
             # a width of 750px (to speedup processing)
-            rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             rgb = imutils.resize(frame, width=args["resolution"])
-            r = frame.shape[1] / float(rgb.shape[1])
 
             # detect the (x, y)-coordinates of the bounding boxes
             # corresponding to each face in the input frame, then compute
@@ -87,13 +90,13 @@ class FacialRecognition:
                     # find the indexes of all matched faces then initialize a
                     # dictionary to count the total number of times each face
                     # was matched
-                    matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                    matched_indexes = [i for (i, b) in enumerate(matches) if b]
                     counts = {}
 
                     # loop over the matched indexes and maintain a count for
                     # each recognized face face
-                    for i in matchedIdxs:
-                        name = data["names"][i]
+                    for index in matched_indexes:
+                        name = data["names"][index]
                         counts[name] = counts.get(name, 0) + 1
 
                     # determine the recognized face with the largest number
@@ -105,23 +108,10 @@ class FacialRecognition:
                 names.append(name)
 
             # loop over the recognized faces
-            for ((top, right, bottom, left), name) in zip(boxes, names):
-                # rescale the face coordinates
-                top = int(top * r)
-                right = int(right * r)
-                bottom = int(bottom * r)
-                left = int(left * r)
-
-                # draw the predicted face name on the image
-                # cv2.rectangle(frame, (left, top), (right, bottom),
-                # (0, 255, 0), 2)
-                # y = top - 15 if top - 15 > 15 else top + 15
-                # cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-                #	0.75, (0, 255, 0), 2)
-
+            for ((_, _, _, _), name) in zip(boxes, names):
                 # print to console, identified person
                 print('Person found: {}'.format(name))
-                
+
                 # Set a flag to sleep the cam for fixed time
                 time.sleep(3.0)
                 self.__username = name
@@ -151,12 +141,18 @@ class FacialRecognition:
 
             # do a bit of cleanup
         cv2.destroyAllWindows()
-        vs.stop()
+        video_stream.stop()
 
         # check to see if the video writer point needs to be released
         if writer is not None:
             writer.release()
 
     def get_username(self):
+        """Get user name using facial recognition
+
+        Returns:
+            str: username that matches the face
+
+        """
         self.recognition()
         return self.__username
