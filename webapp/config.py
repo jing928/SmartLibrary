39a1a4bdd875790ff4ba504e750dbc47aae03f8d
Quan Cheng <s3181520@student.rmit.edu.au>
2019-05-24 10:43:11 +1000

    Adjust visual page to use new data dashboard

--- webapp/config.py
+++ webapp/config.py
@@ -20,8 +20,8 @@ class Config(object):
     # 'DATABASE': 'SmartLibrary'
     # }
 
-    LEND_URL = 'https://datastudio.google.com/embed/reporting/1ni-4ZoFqrTgkxDg1SVKgw7r2gUkvQIgh/page/yYBq'
-
+    # LEND_URL = 'https://datastudio.google.com/embed/reporting/1ni-4ZoFqrTgkxDg1SVKgw7r2gUkvQIgh/page/yYBq'    # v0
+    # LEND_URL = 'https://datastudio.google.com/embed/reporting/14OJZfPlkPp7RNTERAnLz7PnCLPYmvCn2/page/9SZq'	# v1
     RETURN_URL = 'https://datastudio.google.com/embed/reporting/1xNEctzAzy9-LU1GsN1QKZp7UL6q5Av6c/page/vHPq'
-
     # RETURN_URL = 'https://datastudio.google.com/open/1xNEctzAzy9-LU1GsN1QKZp7UL6q5Av6c'
+    LEND_URL = 'https://datastudio.google.com/embed/reporting/1qguKydcbyhQu43qDTeirGt7hBZ3BEwnM/page/VgD'
