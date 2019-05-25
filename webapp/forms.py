c885380c0ad9dcfc8149106f82cee3e19d48c04a
Jing Li <lyovse@gmail.com>
2019-05-25 17:23:34 +1000

    Format form classes

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, DateTimeField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField
 from wtforms.validators import DataRequired, Length
 
 
@@ -15,19 +15,14 @@ class AddBookForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
     pubdate = StringField('Published Date', validators=[DataRequired()])
-    # status_tick = SelectField('Status', choices=[('avail', 'Available'), ('unavail', 'Unavailable')])
 
-    # status = BooleanField('Status')
     submit = SubmitField('Add Book')
 
 
 class EditBookForm(FlaskForm):
-    # bookid = IntegerField('Book ID', validators=[DataRequired()])
     isbn = StringField('ISBN', [DataRequired(), Length(min=5, max=20)])
     title = StringField('Title', validators=[DataRequired()])
     author = StringField('Author', validators=[DataRequired()])
     pubdate = DateTimeField('PublishedDate', format='%Y-%m-%d', validators=[DataRequired()])
-    # status_tick = SelectField('Status', choices=[('avail', 'Available'), ('unavail', 'Unavailable')])
 
-    # status = BooleanField('Status')
     submit = SubmitField('Apply Change')
