4ec0ccfeb8704d1b7238543675013bce4a14d2b3
Jing Li <lyovse@gmail.com>
2019-05-25 23:04:22 +1000

    Do not use Mock

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,24 +1,21 @@
 import unittest
-from unittest import mock
-from rp_app.user_login import UserLogin
 from rp_app.login_with_password import LoginWithPassword
 
 
 class TestLogin(unittest.TestCase):
 
     def setUp(self):
-        self.user_login = UserLogin()
-        self.login_with_password = LoginWithPassword
+        self.login_with_password = LoginWithPassword(ip_dict={'ip': 'test'})
 
     def test_login_with_wrong_username(self):
-        self.login_with_password._LoginWithPassword__password = mock(return_value='abc123')
-        self.login_with_password._LoginWithPassword__username = mock(return_value='youtube')
-        self.assertFalse(self.login_with_password.login_with_password(), False)
+        self.login_with_password._LoginWithPassword__password = 'abc123'
+        self.login_with_password._LoginWithPassword__username = 'youtube'
+        self.assertFalse(self.login_with_password.login_with_password())
 
     def test_login_with_wrong_password(self):
-        self.login_with_password._LoginWithPassword__password = mock(return_value='abcdefg')
-        self.login_with_password._LoginWithPassword__username = mock(return_value='flydog')
-        self.assertFalse(self.login_with_password.login_with_password(), False)
+        self.login_with_password._LoginWithPassword__password = 'abcdefg'
+        self.login_with_password._LoginWithPassword__username = 'flydog'
+        self.assertFalse(self.login_with_password.login_with_password())
 
 
 if __name__ == '__main__':
